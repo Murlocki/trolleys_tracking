f82a62ad8cab2e04e83a730e54bc14a79a665ad5
murlocki <kirillkornilov03@gmail.com>
2025-05-27 02:22:54 +0300

    Удалил лишнии функции и схемы в авторизации

--- backend/src/auth_service/schemas.py
+++ backend/src/auth_service/schemas.py
@@ -1,24 +1,5 @@
 from typing import Optional
-
-from pydantic import BaseModel, EmailStr, Field, AliasChoices
-
-
-class UserCreate(BaseModel):
-    username: str = Field(None, min_length=3, max_length=50, validation_alias=AliasChoices('username', 'userName'))
-    first_name: str = Field(validation_alias=AliasChoices('first_name', 'firstName'))
-    last_name: str = Field(validation_alias=AliasChoices('last_name', 'lastName'))
-    email: EmailStr
-    password: str = Field(min_length=8)
-
-
-class UserUpdate(BaseModel):
-    username: str = Field(None, min_length=3, max_length=50)
-    first_name: str
-    last_name: str
-    email: EmailStr
-    password: str = Field(min_length=8)
-    is_active: bool = Field(True)
-
+from pydantic import BaseModel, Field
 
 class AuthForm(BaseModel):
     identifier: str
